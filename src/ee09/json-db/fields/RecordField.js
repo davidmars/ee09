759e181342bc3be635097e347d71322804e601fc
david <d.marsalone@gmail.com>
2021-04-03 22:22:43 +0200

    oups

--- src/ee09/json-db/fields/RecordField.js
+++ src/ee09/json-db/fields/RecordField.js
@@ -1,4 +1,8 @@
+/**
+ * Une référence à un enregistrement dans la base de données
+ */
 export default class RecordField{
+
     constructor() {
         /**
          * Identifiant unique du DbRecord
@@ -12,9 +16,13 @@ export default class RecordField{
      * @return {DbRecordFile|null}
      */
     get record(){
-        return window.$db.getByUid(this.uid,"file");
+        return window.$db.getByUid(this.uid);
     }
 
+    /**
+     * Définit le record associé
+     * @param {DbRecordFile|null} record
+     */
     set record(record){
         if(this.record){
             this.uid=record.uid;
@@ -22,4 +30,5 @@ export default class RecordField{
             this.uid="";
         }
     }
+
 }
