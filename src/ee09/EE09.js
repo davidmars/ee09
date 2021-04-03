aab35d899d2cca9b420045793eb0b066559c3193
david <d.marsalone@gmail.com>
2021-04-03 10:57:44 +0200

    nettoyage

--- src/ee09/EE09.js
+++ src/ee09/EE09.js
@@ -1,9 +1,6 @@
-import EE09obj from "./utils/EE09obj";
 import EE09urlAnalyzer from "./utils/EE09urlAnalyzer";
 
 export default class EE09{
     constructor() {
-        this.utils=new EE09obj();
-        this.urlAnalyzer=new EE09urlAnalyzer();
     }
 }
